152a16c92de1f2ff4ea6e4dacf5718757a52d0ba
Przemysław Czerkas <pczerkas@gmail.com>
2024-01-27 11:33:44 +0100

    .

--- build/act.sh
+++ build/act.sh
@@ -45,7 +45,7 @@ case "$host_arch" in
     matrix_os="ubuntu-latest"
     ;;
   aarch64)
-    matrix_os="macos-latest"
+    matrix_os="macos-13-xlarge"
     ;;
   *)
     echo "Unsupported host architecture: $host_arch"
